b45ee650cf8e841d4516abe0998c6ce976562cc2
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-04-21 21:39:32 +0300

    application.properties + db

---
+++ src/main/java/seventeam/tgbot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,46 @@
+package seventeam.tgbot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private final TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.stream()
+                .filter(update -> update.message() != null)
+                .forEach(update -> {
+                    Message message = update.message();
+                    Long chatId = message.chat().id();
+                    String text = message.text();
+                    if ("/start".equals(text)) {
+                        sendMassage(chatId, "Бот запущен");
+                    }
+                });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void sendMassage(Long chatId, String massage) {
+        SendMessage sendMessage = new SendMessage(chatId, massage);
+        telegramBot.execute(sendMessage);
+    }
+}
--- src/main/java/seventeam/tgbot/model/Dog.java
+++ src/main/java/seventeam/tgbot/model/Dog.java
@@ -3,16 +3,43 @@ package seventeam.tgbot.model;
 import java.util.Objects;
 
 public class Dog extends Pet {
+
+    private final long id;
     private final String name;
     private final String breed;
     private final int age;
     private final String suit;
 
-    public Dog(String name, String breed, int age, String suit) {
+    public Dog(long id, String name, String breed, int age, String suit) {
+        this.id = id;
         this.name = name;
         this.breed = breed;
         this.age = age;
         this.suit = suit;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getSuit() {
+        return suit;
     }
 
     @Override
@@ -20,21 +47,27 @@ public class Dog extends Pet {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dog dog = (Dog) o;
-        return age == dog.age && name.equals(dog.name) && breed.equals(dog.breed) && suit.equals(dog.suit);
+        return id == dog.id && age == dog.age && name.equals(dog.name) && breed.equals(dog.breed) && suit.equals(dog.suit);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, breed, age, suit);
+        return Objects.hash(id, name, breed, age, suit);
     }
 
     @Override
     public String toString() {
         return "Dog{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", breed='" + breed + '\'' +
                 ", age=" + age +
                 ", suit='" + suit + '\'' +
                 "} ";
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog(1, "Leo", "Taxe", 7, "brown leopard");
+        System.out.println(dog.getName());
     }
 }
