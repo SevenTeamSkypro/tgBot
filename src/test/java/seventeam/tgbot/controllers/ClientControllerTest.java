1236fbe95d9b8bdea0634f46e65fc8478ae8e3e2
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-06-05 17:40:43 +0300

    PUT test

--- src/test/java/seventeam/tgbot/controllers/ClientControllerTest.java
+++ src/test/java/seventeam/tgbot/controllers/ClientControllerTest.java
@@ -1,5 +1,6 @@
 package seventeam.tgbot.controllers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,15 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import seventeam.tgbot.model.Client;
+import seventeam.tgbot.repository.ClientRepository;
 import seventeam.tgbot.service.impl.ClientServiceImpl;
+
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,10 +27,13 @@ class ClientControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private ClientServiceImpl clientService;
-
+    @MockBean
+    private ClientRepository clientRepository;
+    @MockBean
     Client client = new Client(0L, 0L, "FirstName", "LastName", "7_xxx_xxx_xx_xx");
 
     @Test
+    @DisplayName("Проверка получения клиента по chatId")
     void getClient() throws Exception {
         when(clientService.getUserByChatId(0L)).thenReturn(client);
         mockMvc.perform(get("/c/get").param("chatId", "0"))
@@ -37,11 +43,25 @@ class ClientControllerTest {
     }
 
     @Test
-    void getAll() {
+    @DisplayName("Проверка получения всех клиентов")
+    void getAll() throws Exception {
+        when(clientService.getAll()).thenReturn(List.of(client));
+        mockMvc.perform(get("/c/all"))
+                .andExpect(status().isOk());
+        verify(clientService).getAll();
     }
 
     @Test
-    void updateClient() {
+    void updateClient() throws Exception {
+
+        mockMvc.perform(put("/c/put")
+                        .param("id", "0")
+                        .param("chatId", "0")
+                        .param("firstName", "FirstName")
+                        .param("lastName", "LastName")
+                        .param("phoneNumber", "7_xxx_xxx_xx_xx"))
+                .andExpect(status().isOk());
+        verify(clientService).updateUser(client);
     }
 
     @Test
