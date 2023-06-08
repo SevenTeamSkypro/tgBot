b068e8969c307f721ceb9723b87cfd5c2b6f9bc2
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-06-08 23:17:45 +0300

    +CatControllerTest

---
+++ src/test/java/seventeam/tgbot/controllers/CatControllerTest.java
@@ -0,0 +1,69 @@
+package seventeam.tgbot.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import seventeam.tgbot.dto.CatDto;
+import seventeam.tgbot.service.impl.CatServiceImpl;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CatControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private CatServiceImpl catService;
+    CatDto catDto = new CatDto("Name", "breed", LocalDate.of(2000, 12, 31), "suit", "gender");
+
+    @Test
+    @DisplayName("Проверка создания питомца")
+    void createCat() throws Exception {
+        mockMvc.perform(post("/cat/new")
+                        .content(objectMapper.writeValueAsString(catDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        verify(catService).createCat("Name", "breed", LocalDate.of(2000, 12, 31), "suit", "gender");
+    }
+
+    @Test
+    @DisplayName("Проверка получения питомца по id")
+    void getCat() throws Exception {
+        when(catService.getCat(0L)).thenReturn(catDto);
+        mockMvc.perform(get("/cat/get").param("id", "0"))
+                .andExpect(status().isOk());
+        verify(catService).getCat(0L);
+    }
+
+    @Test
+    @DisplayName("Проверка получения всех питомцев")
+    void getAll() throws Exception {
+        when(catService.getAllCats()).thenReturn(List.of(catDto));
+        mockMvc.perform(get("/cat/all"))
+                .andExpect(status().isOk());
+        verify(catService).getAllCats();
+    }
+
+    @Test
+    @DisplayName("Проверка удаления питомца по id")
+    void deleteCat() throws Exception {
+        mockMvc.perform(delete("/cat/del").param("id", "0"))
+                .andExpect(status().isOk());
+        verify(catService).deleteCat(0L);
+    }
+}
